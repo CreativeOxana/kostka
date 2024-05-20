d829ae5ff6c53af647bbad5c9c38512e4edf8bf4
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-20 17:47:33 +0200

    Added const throw dice

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+let numberOnDice = 1;
+const dice = document.querySelector('.dice');
+const throwDice = () => {
+  numberOnDice = numberOnDice + 1;
+  if (numberOnDice === 7) {
+    numberOnDice = 1;
+  }
+  dice.src = `img/side${numberOnDice}.svg`;
+};
+document.addEventListener('keydown', throwDice);
